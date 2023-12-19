256a6f9a04314c5bdae62bcce7ba4ac423f7cdb1
Jonathan Machado <jonathan.machado@datadoghq.com>
2023-12-19 13:34:11 +0100

    Change description to align on case

--- modules/agentless-scanner-role/main.tf
+++ modules/agentless-scanner-role/main.tf
@@ -22,7 +22,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
 resource "aws_iam_role" "role" {
   name        = var.iam_role_name
   path        = var.iam_role_path
-  description = "Role used by the Datadog Agentless scanner instance"
+  description = "Role used by the Datadog agentless scanner instance"
 
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 
