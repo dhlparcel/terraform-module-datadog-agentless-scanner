b60d3767c501c86828f595be8436c48ffe041bf0
Jonathan Machado <jonathan.machado@datadoghq.com>
2023-12-20 16:34:23 +0100

    Bump the version of the scanner

--- modules/user_data/variables.tf
+++ modules/user_data/variables.tf
@@ -26,7 +26,7 @@ variable "site" {
 variable "scanner_version" {
   description = "Specifies the agentless scanner version installed"
   type        = string
-  default     = "50.0~rc.7~agentless~scanner~2023121801"
+  default     = "50.0~agentless~scanner~2023122001"
   nullable    = false
 }
 
