2c5fd033e2377f78a5dd7b5b0556bc3c2fd02d43
David du Colombier <djc@datadoghq.com>
2024-01-15 14:45:39 +0100

    Bump agentless-scanner version to 7.50-agentless-scanner-2024010901

--- modules/user_data/variables.tf
+++ modules/user_data/variables.tf
@@ -26,7 +26,7 @@ variable "site" {
 variable "scanner_version" {
   description = "Specifies the agentless scanner version installed"
   type        = string
-  default     = "50.0~agentless~scanner~2023122001"
+  default     = "50.0~agentless~scanner~2024010901"
   nullable    = false
 }
 
