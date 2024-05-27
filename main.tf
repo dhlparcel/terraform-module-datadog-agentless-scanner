34592113babc5e5cf0f8088bc7435395f6976f1e
Pierre Guilleminot <pierre.guilleminot@datadoghq.com>
2024-05-27 15:43:18 +0200

    Fix the main module: passing scanner_channel to the proper module

--- main.tf
+++ main.tf
@@ -8,14 +8,14 @@ module "user_data" {
   source             = "./modules/user_data"
   api_key            = var.api_key
   api_key_secret_arn = var.api_key_secret_arn
+  scanner_channel    = var.scanner_channel
+  scanner_version    = var.scanner_version
   site               = var.site
   tags               = var.tags
 }
 
 module "instance" {
   source               = "./modules/instance"
-  scanner_channel      = var.scanner_channel
-  scanner_version      = var.scanner_version
   user_data            = module.user_data.install_sh
   vpc_id               = module.vpc.vpc.id
   subnet_ids           = [for s in module.vpc.private_subnets : s.id]
